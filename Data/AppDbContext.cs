fb9d35f85dc1964fa4aeefc176b531c5159e27f4
Alexandr <alexandr.kutiev@yandex.ru>
2022-09-06 22:57:12 +0300

    Some refactoring

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -5,10 +5,7 @@ namespace ToDoAPI.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<ToDo> ToDos => Set<ToDo>();
     }
